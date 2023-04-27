ca81bc16dd4907f9ee7ce52744bd45fd3af7bae7
Abhishek <abhishekchandel75900@gmail.com>
2023-04-27 12:55:59 +0530

    SBT Project BUILD

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,11 @@
 object Main {
   def main(args: Array[String]): Unit = {
     println("Hello world!")
-  }
+
+      val maybeString: Option[String] = Some("Hello, world!")
+      val maybeLength: Option[Int] = maybeString.map(_.length)
+
+      println(maybeLength)
+    }
+
 }
